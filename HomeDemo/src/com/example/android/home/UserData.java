be2f44521a00fe8660bfec745d072a5597bb018c
justvisiting <hiteshchaudhary@gmail.com>
2014-03-14 08:39:23 -0700

    Added real package names

--- HomeDemo/src/com/example/android/home/UserData.java
+++ HomeDemo/src/com/example/android/home/UserData.java
@@ -11,11 +11,19 @@ public class UserData {
 	public String[] favpackNames;
 	public String[] suggestPackageNames;
 	
-	private static String[] page1fav = {"com.google.maps", "com.google.phone"}; //if any of them is not installed, skip it
-	private static String[] page1sug = {"com.google.skype", "com.google.outlook"}; //if any of them is not installed skip it
+	private static String[] fav = {"com.android.chrome", "com.google.android.dialer"}; //if any of them is not installed, skip it
+	private static String[] page1sug = {"com.facebook.katana"
+		, "com.google.android.apps.docs"
+		, "com.google.android.gm"
+		, "com.example.android.home"
+		, "com.google.android.googlequicksearchbox"
+		, "com.meetup"
+		, "com.tul.aviate"
+		, "com.estrongs.android.pop"
+
+	}; //if any of them is not installed skip it
 	
-	private static String[] page2fav = {"com.google.maps", "com.google.phone"}; //if any of them is not installed, skip it
-	private static String[] page2sug = {"com.google.skype", "com.google.outlook"}; //if any of them is not installed skip it
+	private static String[] page2sug = {"com.whatsapp", "com.example.android.home", "com.meetup"}; //if any of them is not installed skip it
 	
 	public static ArrayList<UserData> GetUserPages()
 	{
@@ -23,13 +31,13 @@ public class UserData {
 		UserData page1 = new UserData();
 		page1.backgrouncColor = color.holo_blue_light;
 		page1.imageName = "page1.jpg"; //assume we have that in resource folder
-		page1.favpackNames = page1fav; //if any of them is not installed, skip it
+		page1.favpackNames = fav; //if any of them is not installed, skip it
 		page1.suggestPackageNames = page1sug; //if any of them is not installed skip it
 		
 		UserData page2 = new UserData();
 		page2.backgrouncColor = color.holo_blue_light;
 		page2.imageName = "page1.jpg"; //assume we have that in resource folder
-		page2.favpackNames = page1fav; //if any of them is not installed, skip it
+		page2.favpackNames = fav; //if any of them is not installed, skip it
 		page2.suggestPackageNames = page1sug; //if any of them is not installed skip it
 		
 		rv.add(page1);
